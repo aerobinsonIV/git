661af62c4a7fd255c7fbbdb4b3796c1372085691
Iv Robinson <aerobinsoniv@gmail.com>
2021-12-06 22:56:39 -0500

    Inserts years into databases for matched games

--- create_database.py
+++ create_database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from tqdm import tqdm
 
 import harrys_game_list_parser as parser
 import game_title_matcher as matcher
@@ -14,41 +15,48 @@ def replace_quotes(input):
     weird_single_quote = "’"
     return input.replace("'", weird_single_quote)
 
-# Open database
-# connection = sqlite3.connect("games.db")
-# cursor = connection.cursor()
-
-#Load games from Harry's data file
-game_dict = parser.load_games_from_file(games_file)
+# ******************************Database creation***************************************
 
 # Get list of game titles from my file
 with open(titles_file, "r") as game_file:
     game_titles = game_file.readlines()
 
+# Open database
+connection = sqlite3.connect("games.db")
+cursor = connection.cursor()
+
 # Create games table
-# cursor.execute("CREATE TABLE games (title text, developer text, publisher text, genre text, in_archive integer, rarity integer, summary text, notes text)")
+cursor.execute("CREATE TABLE games (title text, year int, developer text, publisher text, genre text, in_archive integer, rarity integer, summary text, notes text)")
+
+for game_title in game_titles:
+    
+    cleaned_game_title = replace_quotes(game_title)[:-1]
+    
+    command = f"insert into games values ('{cleaned_game_title}', '0', '-', '-', '-', 0, 0, '-', '-')"
+    cursor.execute(command)
+
+
+# *****************************************************Title/data matching******************************
+
+#Load games from Harry's data file
+game_dict = parser.load_games_from_file(games_file)
 
 dict_keys = game_dict.keys()
 
-num_items_without_match = 0
-for game_title in game_titles:
+matched_titles_and_keys = []
 
-    matching_key = matcher.find_match(game_title[:-1], dict_keys)
+print("Matching game titles to games...")
+for i in tqdm(range(0, len(game_titles))):
+
+    matching_key = matcher.find_match(game_titles[i][:-1], dict_keys)
 
     if matching_key:
-        pass
-    else:
-        print(f"No match found for {game_title[:-1]}")  
-    
-    
+        matched_titles_and_keys.append((game_titles[i][:-1], matching_key))
 
-    # cleaned_game_title = replace_quotes(game_title)
-    # # We don't want the newline at the end of the game title
-    # command = f"insert into games values ('{cleaned_game_title[:-1]}', '-', '-', '-', 0, 0, '-', '-')"
-    # print(command)
-    # cursor.execute(command)
-
+for pair in matched_titles_and_keys:
+    year_command = f"update games set year = {game_dict[pair[1]][1]} where title = '{pair[0]}'"
+    cursor.execute(year_command)
 
 # Close out
-# connection.commit()
-# connection.close()
+connection.commit()
+connection.close()
