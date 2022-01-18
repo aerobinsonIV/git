4115de5cec722ff7f1e511c026ff6943dbb22f72
Iv <aerobinsoniv@gmail.com>
2022-01-18 14:24:24 -0500

    summary_getter checks through summaries already found and lists ones left to do

--- summary_getter.py
+++ summary_getter.py
@@ -6,40 +6,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-driver = webdriver.Chrome()
-gameSite = "https://gamefaqs.gamespot.com/search?game="
-driver.get(gameSite)
+# driver = webdriver.Chrome()
+# gameSite = "https://gamefaqs.gamespot.com/search?game="
+# driver.get(gameSite)
 
 
 gameFile = open("./games_in_db.txt", "r")
-outputFile = open("./game_descriptions.txt", "a")
-rOutputFile = open("./game_descriptions.txt", "r")
+outputFile = open("./game_descriptions_combo.txt", "a")
+rOutputFile = open("./game_descriptions_combo.txt", "r")
 errorFile = open("./game_errors.txt", "w")
 titles = gameFile.readlines()
 
 alreadyDone = []
 for t in rOutputFile.readlines():
     if(t[0] == '>'):
-        alreadyDone.append(t[1:].replace("\n", ""))
+        title = t[1:].replace("\n", "")
+        alreadyDone.append(title)
 rOutputFile.close()
 
 for gameName in titles:
     gameName = gameName.replace("\n", "")
     if(gameName in alreadyDone):
-        pass
-    try:
-        driver.get(gameSite + gameName.replace(" ", "+"))
-        driver.find_elements_by_link_text("PlayStation")[0].click()
+        # print(f"{gameName} already been done")
+        continue
 
-        myElem = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'game_desc')))
+    print(f"{gameName} hasn't been done")
+    # try:
+    #     driver.get(gameSite + gameName.replace(" ", "+"))
+    #     driver.find_elements_by_link_text("PlayStation")[0].click()
 
-        desc = driver.find_element_by_class_name("game_desc")
-        outputFile.write(">{}\n{}\n".format(
-            gameName, desc.text.replace("\n", ".")))
-        outputFile.flush()
-    except Exception as e:
-        print(e)
-        errorFile.write("{}\n".format(gameName))
-        errorFile.flush()
-        pass
+    #     myElem = WebDriverWait(driver, 5).until(
+    #         EC.presence_of_element_located((By.CLASS_NAME, 'game_desc')))
+
+    #     desc = driver.find_element_by_class_name("game_desc")
+    #     outputFile.write(">{}\n{}\n".format(
+    #         gameName, desc.text.replace("\n", ".")))
+    #     outputFile.flush()
+    # except Exception as e:
+    #     print(e)
+    #     errorFile.write("{}\n".format(gameName))
+    #     errorFile.flush()
+    #     pass
